b3802b5a9827aefa727cb21dcda2a5bc6ebdd34b
AndreCosta101 <andre.costa101@gmail.com>
2020-06-05 10:16:31 -0300

    creating repositories

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -1,10 +1,11 @@
 import { Router } from 'express';
-import { startOfHour, parseISO, isEqual } from 'date-fns';
-import Appointment from '../models/Appointment';
+import { startOfHour, parseISO } from 'date-fns';
+
+import AppointmentsRepository from '../repositories/AppointmentsRepository';
 
 const appointmentsRouter = Router();
 
-const appointments: Appointment[] = [];
+const appointmentsRepository = new AppointmentsRepository();
 
 // http://localhost:3333/appointments -> nas rotas
 
@@ -13,8 +14,8 @@ appointmentsRouter.post('/oi', (request, response) => {
 
   const parsedDate = startOfHour(parseISO(date));
 
-  const findAppointmentInSameDate = appointments.find(appointment =>
-    isEqual(parsedDate, appointment.date)
+  const findAppointmentInSameDate = appointmentsRepository.findByDate(
+    parsedDate
   );
 
   if (findAppointmentInSameDate) {
@@ -23,9 +24,7 @@ appointmentsRouter.post('/oi', (request, response) => {
       .json({ message: 'This appointment is already booked' });
   }
 
-  const appointment = new Appointment(provider, parsedDate);
-
-  appointments.push(appointment);
+  const appointment = appointmentsRepository.create(provider, parsedDate);
 
   return response.json(appointment);
 });
