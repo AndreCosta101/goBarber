2721e3f5695d95e962bd57c64b44582f6d854146
AndreCosta101 <andre.costa101@gmail.com>
2020-06-05 08:51:28 -0300

    building appointments

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -1,11 +1,24 @@
 import { Router } from 'express';
+import { uuid } from 'uuidv4';
 
 const appointmentsRouter = Router();
 
-// http://localhost:3333/appointments
+const appointments = [];
 
-appointmentsRouter.post('/', (request, response) => {
-  return response.json({ message: 'hello world' });
+// http://localhost:3333/appointments -> nas rotas
+
+appointmentsRouter.post('/oi', (request, response) => {
+  const { provider, date } = request.body;
+
+  const appointment = {
+    id: uuid(),
+    provider,
+    date,
+  };
+
+  appointments.push(appointment);
+
+  return response.json(appointment);
 });
 
 export default appointmentsRouter;
